1f02f2feb8c4654ddab1d4900448bfb1b254b537
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-04-04 15:52:24 +0200

    function exercises

---
+++ 02_functions/A_exercise/endsWithT.js
@@ -0,0 +1,7 @@
+// Write a function `endsWithT` that accepts a string as an argument. The function should return a
+// boolean indicating whether or not the string ends with the character 't'.
+
+console.log(endsWithT("smart")); // true
+console.log(endsWithT("racket")); // true
+console.log(endsWithT("taco")); // false
+console.log(endsWithT("boomerang")); // false
