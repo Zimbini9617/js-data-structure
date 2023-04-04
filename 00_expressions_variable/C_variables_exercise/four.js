9aae06c367f03d0374762a7f8ebf0d05f008b794
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-04-04 14:09:20 +0200

    initial file

--- 00_expressions_variable/C_variables_exercise/four.js
+++ 00_expressions_variable/C_variables_exercise/four.js
@@ -1,6 +1,6 @@
-let movie = 'Badmom P1';
+let movie = 'Badmoms P1';
 let views = '5';
 
-let str = 'I watched' +movie+ ' about' + views+ ' number of times';
+let str = 'I watched ' +movie+ ' about ' + views+ ' number of times';
 
 console.log(str);
