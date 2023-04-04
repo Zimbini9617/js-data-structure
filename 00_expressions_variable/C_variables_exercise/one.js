cf865bbf3053418f6de2014bf5134afeb3be2e29
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-04-04 13:45:50 +0200

    initial file

---
+++ 00_expressions_variable/C_variables_exercise/one.js
@@ -0,0 +1,12 @@
+let word = "bye";
+console.log(word + " falicia");
+console.log(word);
+
+let num = 10;
+num = num *2;
+console.log(num);
+
+let bottleOfBeer = 99;
+let around = bottleOfBeer - 1;
+console.log(around);
+console.log(bottleOfBeer);
