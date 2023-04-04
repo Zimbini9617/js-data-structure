a648c0c4a6b1ef148cbc3e22278096507fcc962a
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-04-04 14:55:38 +0200

    initial file

---
+++ 01_conditionals_exercise/two/snippet-2-2.js
@@ -0,0 +1,26 @@
+let q = 25;
+if (q % 3 === 0 && q % 5 === 0) {
+  console.log("both");
+} else if (q % 3 === 0 || q % 5 == 0) {
+  console.log("either");
+} else {
+  console.log("neither");
+}
+
+let r = 9;
+if (r % 3 === 0 && r % 5 === 0) {
+  console.log("both");
+} else if (r % 3 === 0 || r % 5 == 0) {
+  console.log("either");
+} else {
+  console.log("neither");
+}
+
+let s = 15;
+if (s % 3 === 0 && s % 5 === 0) {
+  console.log("both");
+} else if (s % 3 === 0 || s % 5 == 0) {
+  console.log("either");
+} else {
+  console.log("neither");
+}
